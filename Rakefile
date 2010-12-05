dadea1c428aec12ebefd5a03c211a48ff40f924b
Michael Prins <michael.prins@me.com>
2010-12-05 13:35:30 +0200

    Refactored the lib folder structure. Fleshed out the spec for Bitcoin::Client.validateaddress. Changed the format for spec output.

--- Rakefile
+++ Rakefile
@@ -15,22 +15,27 @@ Jeweler::Tasks.new do |gem|
   gem.name = "bitcoin"
   gem.homepage = "http://github.com/requnix/bitcoin"
   gem.license = "MIT"
-  gem.summary = %Q{Ruby library for communicating with a BitCoin client}
-  gem.description = %Q{The BitCoin RubyGem intends to simplify the process of communicating with and managing a BitCoin client on the same server by issuing commands to it via JSON-RPC. It will also include basic account management and transaction feedback.}
+  gem.summary = %Q{Ruby library for communicating with a Bitcoin client}
+  gem.description = %Q{The Bitcoin RubyGem intends to simplify the process of communicating with and managing a BitCoin client on the same server by issuing commands to it via the bitcoind CLI. It will also include basic account management and transaction feedback.}
   gem.email = "michael.prins@me.com"
-  gem.authors = ["Michael Prins"]
+  gem.author = "Michael Prins"
+  gem.requirements << "An instance of the bitcoin client running where the gem will be loaded"
   
   # Include your dependencies below. Runtime dependencies are required when using your gem,
   # and development dependencies are only needed for development (ie running rake tasks, tests, etc)
   gem.add_runtime_dependency 'json'
-  #  gem.add_development_dependency 'rspec', '> 1.2.3'
+  
+  gem.add_development_dependency 'rspec', '~> 2.2.0'
+  gem.add_development_dependency 'jeweler', '~> 1.5.1'
+  gem.add_development_dependency 'simplecov', '>= 0.3.5'
 end
 Jeweler::RubygemsDotOrgTasks.new
 
 require 'rspec/core'
 require 'rspec/core/rake_task'
-RSpec::Core::RakeTask.new(:spec) do |spec|
-  spec.pattern = FileList['spec/**/*_spec.rb']
+RSpec::Core::RakeTask.new(:spec) do |t|
+  t.pattern = FileList['spec/**/*_spec.rb']
+  t.rspec_opts = ['--format nested', '--color']
 end
 
 task :default => :spec
--- spec/bitcoin_client_spec.rb
+++ spec/bitcoin_client_spec.rb
@@ -1,100 +1,224 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Bitcoin::Client" do
+describe Bitcoin::Client do
   
   describe "calling getinfo" do 
-    
-    it "returns useful information" do
+    before(:all) do
       @response = Bitcoin::Client.getinfo
+    end
+    
+    it "returns a Hash" do
       @response.should be_an_instance_of(Hash)
-      
+    end
+    it "indicates the version of the client" do
       @response.should have_key('version')
       @response['version'].should == 31700
-      
+    end
+    it "indicates the balance on the client" do
       @response.should have_key('balance')
       @response['balance'].should == 84.0
-      
+    end
+    it "indicates the number of blocks downloaded" do
       @response.should have_key('blocks')
       @response['blocks'].should == 90000
-      
+    end
+    it "indicates the current number of connections to peers" do
       @response.should have_key('connections')
       @response['connections'].should == 12
-      
+    end
+    it "indicates the proxy in use if any" do
       @response.should have_key('proxy')
       @response['proxy'].should be_empty
-      
+    end
+    it "indicates whether the client is generating blocks" do
       @response.should have_key('generate')
       @response['generate'].should be_true
-      
+    end
+    it "indicates how many processors the client is allowed to use" do
       @response.should have_key('genproclimit')
       @response['genproclimit'].should == -1
-      
+    end
+    it "indicates the current difficulty prescribed by the network" do
       @response.should have_key('difficulty')
       @response['difficulty'].should == 8032.0
-      
+    end
+    it "indicates how many hash operations the client completes every second" do
       @response.should have_key('hashespersec')
       @response['hashespersec'].should == 750000
-      
+    end
+    it "indicates whether the client is running on the test network" do
       @response.should have_key('testnet')
       @response['testnet'].should be_false
-      
+    end
+    it "indicates the age of the oldest available address in the key pool" do
       @response.should have_key('keypoololdest')
       @response['keypoololdest'].should == 1291276195
-      
+    end
+    it "indicates the transaction fee it adds to every transaction" do
       @response.should have_key('paytxfee')
       @response['paytxfee'].should == 0.0
-      
+    end
+    it "indicates errors if they occur" do
       @response.should have_key('errors')
       @response['errors'].should be_empty
     end
-  end
-  
-  it "should call gethashespersec and return a Fixnum" do
-    @response = Bitcoin::Client.gethashespersec
-    @response.should be_an_instance_of(Fixnum)
-    @response.should == 750000
-  end
-  
-  it "should call getbalance without params and return the total balance as a Float" do
-    @response = Bitcoin::Client.getbalance
-    @response.should be_an_instance_of(Float)
-    @response.should == 84.0
-  end
-  
-  it "should call getblockcount and return the block count as a Fixnum" do
-    @response = Bitcoin::Client.getblockcount
-    @response.should be_an_instance_of(Fixnum)
-    @response.should == 90000
-  end
-  
-  it "should call getconnectioncount and return the number of peers as a Fixnum" do
-    @response = Bitcoin::Client.getconnectioncount
-    @response.should be_an_instance_of(Fixnum)
-    @response.should == 12
-  end
-  
-  it "should call getdifficulty and return the current proof-of-work difficulty as a Float" do
-    @response = Bitcoin::Client.getdifficulty
-    @response.should be_an_instance_of(Float)
-    @response.should == 8032.0
-  end
-  
-  it "should call getgenerate and return whether or not the server is generating bitcoins" do
-    @response = Bitcoin::Client.getgenerate
-    @response.should be_true
-  end
-  
-  it "should be able to validate a Bitcoin address" do
-    @response = Bitcoin::Client.validateaddress '18V4MzDwX7q4548aXQoBeg7kedPzs67FsE'
-    @response.should be_an_instance_of(Hash)
     
-    @response.should have_key('isvalid')
-    @response.should have_key('address')
-    @response.should have_key('ismine')
+  end
+  
+  describe "calling gethashespersec" do
+    before :all do
+      @response = Bitcoin::Client.gethashespersec
+    end
     
-    @response['isvalid'].should be_true
-    @response['ismine'].should be_false
-    @response['address'].should match('18V4MzDwX7q4548aXQoBeg7kedPzs67FsE')
+    it "returns a Fixnum" do
+      @response.should be_an_instance_of(Fixnum)
+    end
+    it "returns the number of hashes currently being generated every second" do
+      @response.should == 750000
+    end
+    
+  end
+  
+  describe "calling getbalance" do
+    
+    describe "without params" do
+      before :all do
+        @response = Bitcoin::Client.getbalance
+      end
+      
+      it "returns a Float" do
+        @response.should be_an_instance_of(Float)
+      end
+      it "returns the total balance on the client" do
+        @response.should == 84.0
+      end
+      
+    end
+    
+  end
+  
+  describe "calling getblockcount" do
+    before :all do
+      @response = Bitcoin::Client.getblockcount
+    end
+    
+    it "returns a Fixnum" do
+      @response.should be_an_instance_of(Fixnum)
+    end
+    it "returns the total block count downloaded" do
+      @response.should == 90000
+    end
+    
+  end
+  
+  describe "calling getconnectioncount" do
+    before :all do
+      @response = Bitcoin::Client.getconnectioncount
+    end
+    
+    it "returns a Fixnum" do
+      @response.should be_an_instance_of(Fixnum)
+    end
+    it "returns the number of peers the client is currently connected to" do
+      @response.should == 12
+    end
+  end
+  
+  describe "calling getdifficulty" do
+    before :all do
+      @response = Bitcoin::Client.getdifficulty
+    end
+    
+    it "returns a Float" do
+      @response.should be_an_instance_of(Float)
+    end
+    it "returns the current proof-of-work difficulty" do
+      @response.should == 8032.0
+    end
+  
+  end
+  
+  describe "calling getgenerate" do
+    before :all do
+      @response = Bitcoin::Client.getgenerate
+    end
+    
+    it "returns whether or not the server is generating bitcoins" do
+      @response.should be_true
+    end
+  
+  end
+  
+  describe "calling validateaddress" do
+    
+    describe "with an invalid address" do
+      before :all do
+        @response = Bitcoin::Client.validateaddress 'abcdefghijklmnpqrstuvwxyzABCDEFGHJ'
+      end
+      
+      it "returns a Hash" do
+        @response.should be_an_instance_of(Hash)
+      end
+      it "specifies that the address is not valid" do
+        @response.should have_key('isvalid')
+        @response['isvalid'].should be_false
+      end
+      it "does not return the invalid address" do
+        @response.should_not have_key('address')
+      end
+      
+    end
+    
+    describe "with a valid address" do
+      
+      describe "that is not in the wallet" do
+        before :all do
+          @response = Bitcoin::Client.validateaddress '18V4MzDwX7q4548aXQoBeg7kedPzs67FsE'
+        end
+        
+        it "returns a Hash" do
+          @response.should be_an_instance_of(Hash)
+        end
+        it "returns the address" do
+          @response.should have_key('address')
+          @response['address'].should match('18V4MzDwX7q4548aXQoBeg7kedPzs67FsE')
+        end
+        it "specifies that the address is valid" do
+          @response.should have_key('isvalid')
+          @response['isvalid'].should be_true
+        end
+        it "specifies that the address is not in the wallet" do
+          @response.should have_key('ismine')
+          @response['ismine'].should be_false
+        end
+        
+      end
+      
+      describe "that is in the wallet" do
+        before :all do
+          @response = Bitcoin::Client.validateaddress '1HkiBfDUL1e8Cbb8Q34asW1mUGybNKwkpU'
+        end
+        
+        it "returns a Hash" do
+          @response.should be_an_instance_of(Hash)
+        end
+        it "returns the address" do
+          @response.should have_key('address')
+          @response['address'].should match('1HkiBfDUL1e8Cbb8Q34asW1mUGybNKwkpU')
+        end
+        it "specifies that the address is valid" do
+          @response.should have_key('isvalid')
+          @response['isvalid'].should be_true
+        end
+        it "specifies that the address is in the wallet" do
+          @response.should have_key('ismine')
+          @response['ismine'].should be_true
+        end
+        
+      end
+      
+    end
+  
   end
   
 end
